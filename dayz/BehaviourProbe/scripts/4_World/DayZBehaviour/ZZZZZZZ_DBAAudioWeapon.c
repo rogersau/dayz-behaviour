ef6b7d3ff72108a34e29be6dbccb96d12173148d
James R <rogers.man@gmail.com>
2026-07-23 16:09:52 +1000

    feat: capture authoritative gunshot opportunities

---
+++ dayz/BehaviourProbe/scripts/4_World/DayZBehaviour/ZZZZZZZ_DBAAudioWeapon.c
@@ -0,0 +1,37 @@
+modded class Weapon_Base
+{
+    override void EEFired(int muzzleType, int mode, string ammoType)
+    {
+        super.EEFired(muzzleType, mode, ammoType);
+
+        if (!GetGame().IsServer())
+        {
+            return;
+        }
+
+        PlayerBase owner = PlayerBase.Cast(GetHierarchyRootPlayer());
+        if (!owner || !owner.GetIdentity())
+        {
+            return;
+        }
+
+        DBAProbeCombatEvent eventData = new DBAProbeCombatEvent;
+        eventData.event_type = "SHOT_FIRED_SERVER";
+        eventData.server_time_ms = GetGame().GetTime();
+        eventData.source_player_id = owner.GetIdentity().GetId();
+        eventData.source_type = GetType();
+        eventData.weapon_type = GetType();
+        eventData.ammo = ammoType;
+        eventData.muzzle_index = muzzleType;
+        eventData.world_position = owner.GetPosition();
+
+        ItemBase suppressor = GetAttachedSuppressor();
+        if (suppressor)
+        {
+            eventData.is_suppressed = true;
+            eventData.suppressor_type = suppressor.GetType();
+        }
+
+        DBAProbeRuntime.QueueCombatEvent(eventData);
+    }
+};
