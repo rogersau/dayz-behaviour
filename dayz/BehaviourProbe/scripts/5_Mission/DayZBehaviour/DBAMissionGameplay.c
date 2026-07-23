7af188b0d33a9fa341d999199c61d32a92c33605
James R <rogers.man@gmail.com>
2026-07-23 11:04:26 +1000

    feat: add 10Hz edge detector and pre-event ring export

--- dayz/BehaviourProbe/scripts/5_Mission/DayZBehaviour/DBAMissionGameplay.c
+++ dayz/BehaviourProbe/scripts/5_Mission/DayZBehaviour/DBAMissionGameplay.c
@@ -1,6 +1,7 @@
 modded class MissionGameplay
 {
-    protected float m_DBAProbeSampleAccumulator;
+    protected float m_DBAProbeDetectorAccumulator;
+    protected float m_DBAProbeBaselineAccumulator;
     protected float m_DBAProbeBatchAccumulator;
     protected int m_DBAProbeClientSequence;
     protected int m_DBAProbeBatchSequence;
@@ -10,7 +11,11 @@ modded class MissionGameplay
     protected int m_DBAProbeBatchesAttempted;
     protected int m_DBAProbeEdgesAttempted;
     protected int m_DBAProbeLastHealthMS;
+    protected int m_DBAProbeLastDetectorMS;
+    protected int m_DBAProbeBurstUntilMS;
+    protected int m_DBAProbeLastQueuedSequence;
     protected ref array<ref DBAClientSample> m_DBAProbeSamples = new array<ref DBAClientSample>;
+    protected ref array<ref DBAClientSample> m_DBAProbeRing = new array<ref DBAClientSample>;
     protected ref DBAClientTransitionDetector m_DBAProbeTransitionDetector = new DBAClientTransitionDetector;
 
     override void OnUpdate(float timeslice)
@@ -20,25 +25,18 @@ modded class MissionGameplay
         PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
         if (!player || !player.IsAlive() || player.IsUnconscious())
         {
-            m_DBAProbeSamples.Clear();
-            m_DBAProbeSampleAccumulator = 0;
-            m_DBAProbeBatchAccumulator = 0;
+            ResetDBALocalCollectorState();
             return;
         }
 
-        float interval = 0.5;
-        if (player.IsFireWeaponRaised() || player.IsInIronsights() || player.IsInOptics())
-        {
-            interval = 0.1;
-        }
-
-        m_DBAProbeSampleAccumulator += timeslice;
+        m_DBAProbeDetectorAccumulator += timeslice;
+        m_DBAProbeBaselineAccumulator += timeslice;
         m_DBAProbeBatchAccumulator += timeslice;
 
-        if (m_DBAProbeSampleAccumulator >= interval)
+        if (m_DBAProbeDetectorAccumulator >= 0.1)
         {
-            m_DBAProbeSampleAccumulator = 0;
-            CaptureDBAProbeSample(player);
+            m_DBAProbeDetectorAccumulator = 0;
+            CaptureDBADetectorSample(player);
         }
 
         if (m_DBAProbeBatchAccumulator >= 1.0 || m_DBAProbeSamples.Count() >= DBAProbeConstants.MAX_CLIENT_SAMPLES_PER_BATCH)
@@ -49,7 +47,20 @@ modded class MissionGameplay
         SendDBAProbeHealth();
     }
 
-    protected void CaptureDBAProbeSample(PlayerBase player)
+    protected void ResetDBALocalCollectorState()
+    {
+        m_DBAProbeSamples.Clear();
+        m_DBAProbeRing.Clear();
+        m_DBAProbeDetectorAccumulator = 0;
+        m_DBAProbeBaselineAccumulator = 0;
+        m_DBAProbeBatchAccumulator = 0;
+        m_DBAProbeLastDetectorMS = 0;
+        m_DBAProbeBurstUntilMS = 0;
+        m_DBAProbeLastQueuedSequence = 0;
+        m_DBAProbeTransitionDetector = new DBAClientTransitionDetector;
+    }
+
+    protected void CaptureDBADetectorSample(PlayerBase player)
     {
         DBAClientSample sample = new DBAClientSample;
         m_DBAProbeSamplesCaptured++;
@@ -62,39 +73,82 @@ modded class MissionGameplay
         sample.is_in_ironsights = player.IsInIronsights();
         sample.is_in_optics = player.IsInOptics();
         sample.is_in_third_person = player.IsInThirdPerson();
-        sample.sample_mode = "baseline";
-        if (sample.is_weapon_raised || sample.is_in_ironsights || sample.is_in_optics)
-        {
-            sample.sample_mode = "ready";
-        }
+        sample.sample_mode = "detector";
 
         EntityAI itemInHands = player.GetItemInHands();
         if (itemInHands)
         {
             sample.item_in_hands_type_id = itemInHands.GetType();
         }
-
         DBAProbeRuntime.ConsumeLocalShot(sample.local_shot_count, sample.local_shot_muzzle_index);
 
+        if (m_DBAProbeRing.Count() >= 50)
+        {
+            m_DBAProbeRing.RemoveOrdered(0);
+        }
+        m_DBAProbeRing.Insert(sample);
+
+        int lowerMS = m_DBAProbeLastDetectorMS;
+        if (lowerMS <= 0)
+        {
+            lowerMS = Math.Max(0, sample.client_monotonic_time_ms - 100);
+        }
         array<string> transitions = new array<string>;
         m_DBAProbeTransitionDetector.Process(sample, transitions);
+        bool meaningfulEdge;
         foreach (string transition : transitions)
         {
             if (transition == "WEAPON_RAISED" || transition == "ADS_ENTERED" || transition == "OPTICS_ENTERED" || transition == "DELIBERATE_CAMERA_TURN" || transition == "SHOT_FIRED_CLIENT")
             {
-                SendDBADecisionEdge(transition, sample.client_monotonic_time_ms, sample.camera_direction);
+                meaningfulEdge = true;
+                m_DBAProbeBurstUntilMS = sample.client_monotonic_time_ms + 10000;
+                SendDBATemporalDecisionEdge(transition, lowerMS, sample.client_monotonic_time_ms, sample.camera_direction);
             }
         }
+        if (meaningfulEdge)
+        {
+            QueueDBARingForExport();
+        }
 
+        if (sample.client_monotonic_time_ms <= m_DBAProbeBurstUntilMS)
+        {
+            sample.sample_mode = "burst";
+            QueueDBASampleForExport(sample);
+        }
+        else if (m_DBAProbeBaselineAccumulator >= 0.5)
+        {
+            m_DBAProbeBaselineAccumulator = 0;
+            sample.sample_mode = "baseline";
+            QueueDBASampleForExport(sample);
+        }
+        m_DBAProbeLastDetectorMS = sample.client_monotonic_time_ms;
+    }
+
+    protected void QueueDBARingForExport()
+    {
+        foreach (DBAClientSample sample : m_DBAProbeRing)
+        {
+            sample.sample_mode = "pre_event_ring";
+            QueueDBASampleForExport(sample);
+        }
+    }
+
+    protected void QueueDBASampleForExport(DBAClientSample sample)
+    {
+        if (!sample || sample.client_sequence <= m_DBAProbeLastQueuedSequence)
+        {
+            return;
+        }
         if (m_DBAProbeSamples.Count() >= DBAProbeConstants.MAX_CLIENT_SAMPLES_PER_BATCH)
         {
             m_DBAProbeSamples.RemoveOrdered(0);
             m_DBAProbeDroppedSamples++;
         }
         m_DBAProbeSamples.Insert(sample);
+        m_DBAProbeLastQueuedSequence = sample.client_sequence;
     }
 
-    protected void SendDBADecisionEdge(string edgeType, int clientTimeMS, vector cameraDirection)
+    protected void SendDBATemporalDecisionEdge(string edgeType, int clientLowerMS, int clientUpperMS, vector cameraDirection)
     {
         string nonce = DBAProbeRuntime.GetClientNonce();
         if (nonce == "")
@@ -107,10 +161,11 @@ modded class MissionGameplay
         rpc.Write(DBAProbeConstants.SCHEMA_VERSION);
         rpc.Write(nonce);
         rpc.Write(++m_DBAProbeEdgeSequence);
-        rpc.Write(clientTimeMS);
+        rpc.Write(clientLowerMS);
+        rpc.Write(clientUpperMS);
         rpc.Write(edgeType);
         rpc.Write(cameraDirection);
-        rpc.Send(null, DBAProbeConstants.RPC_DECISION_EDGE, true, null);
+        rpc.Send(null, DBAProbeConstants.RPC_DECISION_EDGE_V2, true, null);
     }
 
     protected void SendDBAProbeBatch()
